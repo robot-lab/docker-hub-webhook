5796faef4763b4338a886eeaabb83e72e90dcd6e
korwin <levozavr@mail.ru>
2018-11-25 16:51:33 +0300

    add proxy for frontend

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	http.HandleFunc("/webhook", handlers.WebHooks)
 	http.HandleFunc("/admin", handlers.Settings)
 	http.HandleFunc("/auth", handlers.Auth)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
